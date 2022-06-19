73edc6365e08cdcb2e2f50699f10525046a9ed8a
king <102906132+Ggy-king@users.noreply.github.com>
2022-06-19 18:03:51 +0800

    Add files via upload

--- miniprogram-1/pages/index/index.js
+++ miniprogram-1/pages/index/index.js
@@ -8,12 +8,14 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    name:'dell',
+    users:[{personal:'dell'},{personal:'lee'}],
+    content:'this is content'
+  },
+  changeName(){
+    this.setData({
+      name:'lee'
+    })
   },
   // 事件处理函数
   bindViewTap() {
@@ -22,30 +24,11 @@ Page({
     })
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+    console.log(wx),
+    wx.showModal({
+      cancelColor: 'black',
     })
   }
+ 
+  
 })
